2e003f1b25e17f8b36c5181e16a3ff2ff3ed47e5
Gaven Robertson <gaven.robertson@gmail.com>
2024-03-11 20:49:10 -0700

    Changed the responsive functionality, currently adding button click handling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 import '../styles/main.scss'
 
 export default function Navbar() {
+    const [handleClick, setHandleClick] = useState(false)
+
+
     return (
         <header>
             <nav className="nav">
@@ -12,6 +15,13 @@ export default function Navbar() {
                     <li className="nav-menu-item">Projects</li>
                     <li className="nav-menu-item">Contact</li>
                 </ul>
+                <button className="hidden-nav-button button-one" aria-controls="primary-navigation" aria-expanded={handleClick}>
+                    <svg className="hamburger" viewBox="0 0 100 100" width="100">
+                        <rect className="line top" width="80" height="10" x="10" y="25" rx="5"></rect>
+                        <rect className="line middle" width="80" height="10" x="10" y="45" rx="5"></rect>
+                        <rect className="line bottom" width="80" height="10" x="10" y="65" rx="5"></rect>
+                    </svg>
+                </button>
             </nav>
         </header>
     )
