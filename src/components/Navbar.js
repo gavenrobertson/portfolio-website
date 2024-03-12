1ddad1cd97245726b8effdcc933d1b7b68a0796c
Gaven Robertson <gaven.robertson@gmail.com>
2024-03-11 21:01:45 -0700

    fixed some of the onClick functionality in Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,9 @@ import '../styles/main.scss'
 export default function Navbar() {
     const [handleClick, setHandleClick] = useState(false)
 
+    const click = () => setHandleClick(!handleClick);
+
+    console.log(handleClick)
 
     return (
         <header>
@@ -15,7 +18,7 @@ export default function Navbar() {
                     <li className="nav-menu-item">Projects</li>
                     <li className="nav-menu-item">Contact</li>
                 </ul>
-                <button className="hidden-nav-button button-one" aria-controls="primary-navigation" aria-expanded={handleClick}>
+                <button onClick={click} className="hidden-nav-button button-one" aria-controls="primary-navigation" aria-expanded={handleClick}>
                     <svg className="hamburger" viewBox="0 0 100 100" width="100">
                         <rect className="line top" width="80" height="10" x="10" y="25" rx="5"></rect>
                         <rect className="line middle" width="80" height="10" x="10" y="45" rx="5"></rect>
