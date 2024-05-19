b0883b0bf95996e7cab32b97a5aa72ef33203142
Gaven Robertson <gaven.robertson@gmail.com>
2024-05-18 22:23:01 -0700

    Added Vercel Analytics

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <>
+    <Analytics/>
     <Navbar/>
     <Home/>
     <About/>
